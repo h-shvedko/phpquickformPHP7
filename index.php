8cda83c2a45085d97cabddff76375227f11bf53d
h-shvedko <hennadii.shvedko@copernicus.org>
2017-06-13 12:45:24 +0200

    CHANGED: index.php to run on PHP7

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?
-phpinfo();
-/*require_once "QuickFormPHP7.php";
+require_once "QuickFormPHP7.php";
 require_once "QuickFormElementPHP7.php";
 
 $form = new QuickFormPHP7('test');
@@ -10,4 +9,4 @@ $form->addElement('html', ['html' => '<h1>Hello world!!!!</h1>']);
 $output = $form->render();
 
 echo $output;
-die;*/
+die;
